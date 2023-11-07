6fc39dddd41c55bea8c54f69d554ca59fb39c2de
Thomas Aleknovic <aleknovic70@gmail.com>
2023-11-07 20:44:01 -0300

    feat: initial config

---
+++ src/main/java/com/thomasaleknovic/worshipteamorganizer/WorshipTeamOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.thomasaleknovic.worshipteamorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshipTeamOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorshipTeamOrganizerApplication.class, args);
+    }
+
+}
