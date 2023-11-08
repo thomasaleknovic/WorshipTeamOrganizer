29d141fda402cee5cc8eb9815d910975d0d9ca68
Thomas Aleknovic <81420616+thomasaleknovic@users.noreply.github.com>
2023-11-08 14:16:06 -0300

    feat: add endpoints to PersonController

--- src/main/java/com/thomasaleknovic/worshipteamorganizer/controllers/PersonController.java
+++ src/main/java/com/thomasaleknovic/worshipteamorganizer/controllers/PersonController.java
@@ -5,6 +5,8 @@ import com.thomasaleknovic.worshipteamorganizer.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,21 @@ public class PersonController {
     @GetMapping
     public ResponseEntity<List<Person>> listAllPerson () {
         return ResponseEntity.ok(personService.findAllPerson());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable UUID id) {
+        return ResponseEntity.ok(personService.findPersonById(id));
+    }
+
+    @PostMapping("/new/person")
+    public ResponseEntity<Person> createPerson (@RequestBody Person person ) {
+        return ResponseEntity.ok(personService.createPerson(person));
+    }
+
+    @PutMapping("/update/person/{id}")
+    public ResponseEntity<Person> updatePerson (@PathVariable UUID id, @RequestBody Person updatedPersonInfo) {
+        Person personToUpdate = personService.getPersonById(id);
+        return ResponseEntity.ok(personService.updatePerson(personToUpdate, updatedPersonInfo))
     }
 }
