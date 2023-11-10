556a128d22fff9792168d3bc35abe4f725fd4101
Thomas Aleknovic <aleknovic70@gmail.com>
2023-11-10 15:56:32 -0300

    feat: changes in PersonRepositoryTest to receive PersonDTO

--- src/test/java/com/thomasaleknovic/worshipteamorganizer/repository/PersonRepositoryTest.java
+++ src/test/java/com/thomasaleknovic/worshipteamorganizer/repository/PersonRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.thomasaleknovic.worshipteamorganizer.repository;
 
+import com.thomasaleknovic.worshipteamorganizer.dtos.PersonDTO;
 import com.thomasaleknovic.worshipteamorganizer.models.Person;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
@@ -23,13 +24,8 @@ class PersonRepositoryTest {
     @Autowired
     PersonRepository personRepository;
 
-    private Person createPerson (Person data) {
-        Person newPerson = new Person(
-                data.getFirstName(),
-                data.getLastName(),
-                data.getBandRole(),
-                data.getIsAdmin()
-        );
+    private Person createPerson (PersonDTO data) {
+        Person newPerson = new Person(data);
          this.entityManager.persist(newPerson);
          return newPerson;
     };
@@ -37,7 +33,7 @@ class PersonRepositoryTest {
     @Test
     @DisplayName("Should return a person by it's id successfully from DB.")
     void findPersonByIdCase1() {
-        Person data = new Person("Thomas", "Aleknovic", "Guitarrista", true);
+        PersonDTO data = new PersonDTO("Thomas", "Aleknovic", "Guitarrista", true);
         Person newPerson = this.createPerson(data);
         Optional<Person> result = this.personRepository.findById(newPerson.getId());
 
