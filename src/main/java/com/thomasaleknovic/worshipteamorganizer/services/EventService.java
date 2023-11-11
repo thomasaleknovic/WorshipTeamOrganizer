7d01eb2e63680082640305f0a0f2e427879c374e
Thomas Aleknovic <aleknovic70@gmail.com>
2023-11-11 12:06:33 -0300

    feat: create EventService

---
+++ src/main/java/com/thomasaleknovic/worshipteamorganizer/services/EventService.java
@@ -0,0 +1,79 @@
+package com.thomasaleknovic.worshipteamorganizer.services;
+
+import com.thomasaleknovic.worshipteamorganizer.dtos.EventDTO;
+import com.thomasaleknovic.worshipteamorganizer.models.Event;
+import com.thomasaleknovic.worshipteamorganizer.models.Person;
+import com.thomasaleknovic.worshipteamorganizer.repository.EventRepository;
+import com.thomasaleknovic.worshipteamorganizer.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+
+    public Event createEvent (EventDTO data) {
+
+        Event event = new Event(data);
+        return eventRepository.save(event);
+
+    }
+
+    public List<Event> findAllEvent() {
+        return eventRepository.findAll();
+    }
+
+    public Optional<Event> findEventById(UUID id) {
+        return eventRepository.findById(id);
+    }
+
+    //Find all events by person id
+    public List<Event> findEventsByPersonId (UUID id) {
+        return eventRepository.findAllByEventTeam_Id(id);
+    }
+
+
+    //Update event info
+
+
+    //Add team member
+
+    public Event addMemberByPersonId (UUID eventId, UUID personId) throws Exception {
+
+         Optional<Event> eventFound = eventRepository.findById(eventId);
+
+         Optional<Person> personFound = personRepository.findById(personId);
+
+            if(personFound.isPresent() && eventFound.isPresent()) {
+                eventFound.get().getEventTeam().add(personFound.get());
+                personFound.get().getEventList().add(eventFound.get());
+                personRepository.save(personFound.get());
+                return eventRepository.save(eventFound.get());
+
+            } else {
+                throw new Exception();
+            }
+
+
+
+    }
+
+    //Remove team member
+
+
+    //Add music
+
+
+    //Remove music
+
+}
